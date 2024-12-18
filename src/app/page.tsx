13197bef7eeb7a404dde15e610aec42d4321bf23
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2024-12-18 23:26:43 +0700

    learn supabase client auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,4 @@
-import { supabaseAdmin } from "@/lib/supabase";
-
+"use client";
 export default function Home() {
-  const setNewView = async () => {
-    const { data, error } = await supabaseAdmin.from("views").insert({
-      name: "random name",
-    });
-
-    if (data) {
-      console.log(data);
-    }
-    if (error) {
-      console.log(error);
-    }
-  };
-
-  setNewView();
   return <div>hello</div>;
 }
